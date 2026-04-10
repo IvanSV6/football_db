072355097e45e9b23029f1a71f19de0d3ae53793
IvanSV6 <iwan.kurskzxcv@gmail.com>
2026-04-10 17:28:59 +0300

    Создан класс SessionManager

---
+++ session.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class SessionManager:
+    def __init__(self):
+        self.current_role = "user"
+
+    def login(self, password):
+        if password == os.getenv("ADMIN_PASSWORD"):
+            self.current_role = "admin"
+            return True
+        elif password == os.getenv("MANAGER_PASSWORD"):
+            self.current_role = "manager"
+            return True
+
+        return False
+    def logout(self):
+        self.current_role = "user"
+
+session = SessionManager()
